7c3dc66549386c0a1ce4d3c581a3bfcd7850f675
Emmydantoch <emmanueltdaniel@yahoo.com>
2024-11-14 15:34:22 +0100

    new

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const submit = () =>{
+    window.location.href='http://127.0.0.1:5500/productpage.html'
+}
+
+// const submit = () =>{
+//     window.location.href='http://127.0.0.1:5500/addedcart.html'
+// }
